585df83d4b91bc9ffb8d6d2effbbdbb06c8ae70a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-26 10:41:28 -0600

    update

--- app/database/includes/header.php
+++ app/database/includes/header.php
@@ -1,10 +1,11 @@
 <div class="header">
     <h3 class="logo ms-3 me-3 pt-1">
-        
         <a class="text-decoration-none text-white" href="/">Job Management System</a>
-        <!-- Set the href attribute to the logout page URL -->
-        <a href="<?php echo BASE_URL; ?>/search.php">Search</a>
+        <!-- Search button -->
+        <a href="<?php echo BASE_URL; ?>/search.php" class="btn btn-outline-light mx-2">Search</a>
+        <!-- Logout button -->
         <a class="float-end" href="index.php?logout=1"><button style="cursor:pointer;" class="btn btn-link text-white"><i class="bi bi-box-arrow-left fs-5"></i></button></a>
     </h3>
 </div>
 <br>
+
