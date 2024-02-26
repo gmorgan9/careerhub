33cfe22c9c337c6312e4fa70c659cf75c089ecdf
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-26 07:43:09 -0600

    update

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once "app/database/connection.php";
 // require_once "app/functions/loggedin.php";
 // require_once "app/functions/logout.php";
 // require_once "app/database/functions.php"
-require_once "app/database/functions.php"
 require_once "path.php";
 session_start();
 
