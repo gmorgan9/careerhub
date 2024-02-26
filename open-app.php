275e4b5d7e16f222adbcdeb9f75149fbc1bc45c7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-26 09:26:09 -0600

    update

--- open-app.php
+++ open-app.php
@@ -37,16 +37,6 @@ if(isLoggedIn() == false) {
 </head>
 <body>
 
-<div class="header">
-        <h3 class="logo ms-3 me-3 pt-1">
-            <a class="text-decoration-none text-white" href="/">Job Management System</a>
-            <a class="float-end" href="index.php?logout='1'"><button style="cursor:pointer;" class="btn btn-link text-white"><i class="bi bi-box-arrow-left fs-5"></i></button></a>
-        </h3>
-    </div>
-<br>
-
-
-
 <?php include(ROOT_PATH . "/app/database/includes/header.php"); ?>
 
     <div class="float-end me-5">
