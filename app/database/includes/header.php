77fc2a6b5f51fac126181743b7e99777787cb1d8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-26 10:42:30 -0600

    update

--- app/database/includes/header.php
+++ app/database/includes/header.php
@@ -2,9 +2,13 @@
     <h3 class="logo ms-3 me-3 pt-1">
         <a class="text-decoration-none text-white" href="/">Job Management System</a>
         <!-- Search button -->
-        <a href="<?php echo BASE_URL; ?>/search.php" class="btn btn-outline-light mx-2">Search</a>
+        
         <!-- Logout button -->
-        <a class="float-end" href="index.php?logout=1"><button style="cursor:pointer;" class="btn btn-link text-white"><i class="bi bi-box-arrow-left fs-5"></i></button></a>
+        <div class="float-end">
+            <a href="<?php echo BASE_URL; ?>/search.php" class="btn btn-outline-light mx-2">Search</a>
+            <a class="" href="index.php?logout=1"><button style="cursor:pointer;" class="btn btn-link text-white"><i class="bi bi-box-arrow-left fs-5"></i></button></a>
+        </div>
+        
     </h3>
 </div>
 <br>
