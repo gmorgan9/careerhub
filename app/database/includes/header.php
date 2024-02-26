361812072972bc0bec287bcacc554e036c37194d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-26 10:46:16 -0600

    update

--- app/database/includes/header.php
+++ app/database/includes/header.php
@@ -5,8 +5,8 @@
         
         <!-- Logout button -->
         <div class="float-end">
-            <a href="<?php echo BASE_URL; ?>/search.php" class="badge text-bg-secondary mx-2 text-decoration-none fs-5" style="">Search</a>
-            <a class="" href="index.php?logout=1"><button style="cursor:pointer;" class="btn btn-link text-white"><i class="bi bi-box-arrow-left fs-6"></i></button></a>
+            <a href="<?php echo BASE_URL; ?>/search.php" class="badge text-bg-secondary mx-2 text-decoration-none fs-6" style="">Search</a>
+            <a class="" href="index.php?logout=1"><button style="cursor:pointer;" class="btn btn-link text-white"><i class="bi bi-box-arrow-left fs-5"></i></button></a>
         </div>
         
     </h3>
