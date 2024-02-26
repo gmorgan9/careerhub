5fede7948b4ea0f17de4b7e605f73ce1e9169b49
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-26 10:51:36 -0600

    update

--- search.php
+++ search.php
@@ -135,7 +135,7 @@ if (isset($_GET['search']) && isset($_GET['search_field'])) {
 <?php if ($result !== false && mysqli_num_rows($result) > 0): ?>
     <?php mysqli_data_seek($result, 0); // Reset result pointer ?>
     <?php while ($row = mysqli_fetch_assoc($result)): ?>
-        <div class="modal fade" id="exampleModal<?php echo $row['app_id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel<?php echo $row['app_id']; ?>" aria-hidden="true">
+        <div class="modal" id="exampleModal<?php echo $row['app_id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel<?php echo $row['app_id']; ?>" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
