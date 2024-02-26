2c77c2e46a017bfc01429f2c5a24ff693e4df52d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-26 08:25:38 -0600

    update

--- app/functions/logout.php
+++ app/functions/logout.php
@@ -3,6 +3,9 @@
 function logoutUser()
     {
         if ($_GET['logout'] == 1) {
+            $uname = $_SESSION['username'];
+            $sql = "UPDATE users SET logged_in='0' WHERE uname='$uname'";
+            mysqli_query($conn, $sql)
             session_destroy();
             header("location: login.php");
             exit; // Prevent further execution
