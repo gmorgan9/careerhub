1a782418b8fd5d48cd920111928fe5fe1b271743
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-26 08:37:05 -0600

    update

--- login.php
+++ login.php
@@ -1,46 +1,54 @@
 <?php
-
+date_default_timezone_set('America/Denver');
 require_once "app/database/connection.php";
 require_once "path.php";
-
 session_start();
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$files = glob("app/functions/*.php");
+foreach ($files as $file) {
+    require_once $file;
+}
 
 // if(isLoggedIn()){
 //    header('location:' . BASE_URL . '/pages/dashboard.php');
 // }
 
-if(isset($_POST['login-btn'])){
-	$user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
-	$fname = mysqli_real_escape_string($conn, $_POST['fname']);
-	$lname = mysqli_real_escape_string($conn, $_POST['lname']);
-	$uname = mysqli_real_escape_string($conn, $_POST['uname']);
-	$email = mysqli_real_escape_string($conn, $_POST['email']);
-    $pass = mysqli_real_escape_string($conn, $_POST['password']);
-	// $pass = md5($_POST['password']);
-	$isadmin = $_POST['account_type'];
-	$loggedin = $_POST['logged_in'];
+// if(isset($_POST['login-btn'])){
+// 	$user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
+// 	$fname = mysqli_real_escape_string($conn, $_POST['fname']);
+// 	$lname = mysqli_real_escape_string($conn, $_POST['lname']);
+// 	$uname = mysqli_real_escape_string($conn, $_POST['uname']);
+// 	$email = mysqli_real_escape_string($conn, $_POST['email']);
+//     $pass = mysqli_real_escape_string($conn, $_POST['password']);
+// 	// $pass = md5($_POST['password']);
+// 	$isadmin = $_POST['account_type'];
+// 	$loggedin = $_POST['logged_in'];
  
-	$select = " SELECT * FROM users WHERE uname = '$uname' && password = '$pass' ";
-	$result = mysqli_query($conn, $select);
-	if(mysqli_num_rows($result) > 0){
-	    $row = mysqli_fetch_array($result);
-	    $sql = "UPDATE users SET logged_in='1' WHERE uname='$uname'";
-	    mysqli_query($conn, $sql);
-	    $_SESSION['fname']          = $row['fname'];
-	    $_SESSION['user_id']        = $row['user_id'];
-	    $_SESSION['loggedin']       = $row['logged_in'];
-	    $_SESSION['user_idno']      = $row['idno'];
-	    $_SESSION['lname']          = $row['lname'];
-	    $_SESSION['acc_type']       = $row['account_type'];
-	    $_SESSION['uname']          = $row['uname'];
-	    $_SESSION['email']          = $row['email'];
-	    $_SESSION['pass']           = $row['password'];
-	    header('location:' . BASE_URL . '/pages/dashboard.php');
-        // header('location: pages/dashboard.php');
-	} else {
-	   $error[] = 'Incorrect username or password!';
-	}
- };
+// 	$select = " SELECT * FROM users WHERE uname = '$uname' && password = '$pass' ";
+// 	$result = mysqli_query($conn, $select);
+// 	if(mysqli_num_rows($result) > 0){
+// 	    $row = mysqli_fetch_array($result);
+// 	    $sql = "UPDATE users SET logged_in='1' WHERE uname='$uname'";
+// 	    mysqli_query($conn, $sql);
+// 	    $_SESSION['fname']          = $row['fname'];
+// 	    $_SESSION['user_id']        = $row['user_id'];
+// 	    $_SESSION['loggedin']       = $row['logged_in'];
+// 	    $_SESSION['user_idno']      = $row['idno'];
+// 	    $_SESSION['lname']          = $row['lname'];
+// 	    $_SESSION['acc_type']       = $row['account_type'];
+// 	    $_SESSION['uname']          = $row['uname'];
+// 	    $_SESSION['email']          = $row['email'];
+// 	    $_SESSION['pass']           = $row['password'];
+// 	    header('location:' . BASE_URL . '/pages/dashboard.php');
+//         // header('location: pages/dashboard.php');
+// 	} else {
+// 	   $error[] = 'Incorrect username or password!';
+// 	}
+//  };
+loginUser($conn);
 ?>
 
 <html>
