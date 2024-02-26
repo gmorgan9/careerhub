c08125bbe68bb62067923fecc95b9a14509e9f50
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-26 09:34:21 -0600

    update

--- app/database/includes/header.php
+++ app/database/includes/header.php
@@ -3,7 +3,8 @@
         
         <a class="text-decoration-none text-white" href="/">Job Management System</a>
         <!-- Set the href attribute to the logout page URL -->
-        <a class="float-end" href="index.php?logout=1"><button style="cursor:pointer;" class="btn btn-link text-black"><i class="bi bi-box-arrow-left fs-5"></i></button></a>
+        <a href="<?php echo BASE_URL; ?>/search.php">Search</a>
+        <a class="float-end" href="index.php?logout=1"><button style="cursor:pointer;" class="btn btn-link text-white"><i class="bi bi-box-arrow-left fs-5"></i></button></a>
     </h3>
 </div>
 <br>
---
+++ search.php
@@ -0,0 +1,82 @@
+<?php
+date_default_timezone_set('America/Denver');
+require_once "app/database/connection.php";
+require_once "path.php";
+session_start();
+
+$files = glob("app/functions/*.php");
+foreach ($files as $file) {
+    require_once $file;
+}
+logoutUser($conn);
+if(isLoggedIn() == false) {
+    header('location:' . BASE_URL . '/login.php');
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/main.css?v=1.77">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <title>Job Management System</title>
+
+    <style>
+        
+    </style>
+    
+</head>
+<body>
+
+<?php include(ROOT_PATH . "/app/database/includes/header.php"); ?>
+
+<h1 class="text-center"><strong>Saerch Applications</strong></h1><br>
+
+<!-- main-container -->
+    <div class="container-fluid main">
+
+        
+        <br>
+        <?php
+            // Pagination links
+            $sql = "SELECT COUNT(*) as total FROM applications WHERE status = 'Applied'";
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_assoc($result);
+            $total_pages = ceil($row["total"] / $limit);
+
+                echo '<ul class="pagination justify-content-center">';
+                for ($i = 1; $i <= $total_pages; $i++) {
+                    $active = ($page == $i) ? "active" : "";
+                    echo "<li class='page-item {$active}'><a class='page-link' href='?page={$i}'>{$i}</a></li>";
+                }
+                echo '</ul>';
+        ?>
+
+    </div>
+<!-- END main-container -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
