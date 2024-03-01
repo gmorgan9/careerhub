ad32ed6d027a9075d75b0f46c0490fd1c8d6b1e1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 13:58:13 -0600

    update all

--- index.php
+++ index.php
@@ -12,10 +12,12 @@ foreach ($files as $file) {
     require_once $file;
 }
 
+
 logoutUser($conn);
 if(isLoggedIn() == false) {
     header('location:' . BASE_URL . '/login.php');
 }
+
 
 ?>
 <!DOCTYPE html>
