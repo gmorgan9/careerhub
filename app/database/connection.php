4a1bed572e575accd90621fda3f8dcbf4d87bdf6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-26 17:29:51 -0500

    update

--- app/database/connection.php
+++ app/database/connection.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $username = "dbuser";
 $password = "DBuser123!";
-$dbname = "application_management";
+$dbname = "careerhub";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
