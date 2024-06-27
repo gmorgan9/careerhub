26f06b04a0d2167f86b598214d8a36f9ddf73464
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 16:53:47 -0500

    update

---
+++ home/projects/offcanvas.php
@@ -0,0 +1,85 @@
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel" style="width: 100vw;">
+                        <div class="portfolio-page-content">
+                            <div class="portfolio-page-wrapper">
+
+                                <nav class="portfolio-page-nav">
+                                  <div class="nav-item portfolio-page-close-button">
+                                      <a href="" id="portfolio-page-close-button" data-bs-dismiss="offcanvas">
+                                          <i class="bi bi-x"></i>
+                                      </a>
+                                  </div>
+                                </nav>
+
+                                <div class="offcanvas-body">
+                                    <div class="portfolio-page-title">
+                                        <h2>Full Project 2</h2>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-12 col-md-8 portfolio-block">
+
+                                        </div>
+                                        <div class="col-sm-12 col-md-4 portfolio-block">
+                                            <div class="project-description">
+                                                <div class="block-title">
+                                                    <h3>Description</h3>
+                                                </div>
+                                                <a href="https://careerhub.morganserver.com/projects/test.php"></a>
+                                                <ul class="project-general-info">
+                                                    <li>
+                                                        <p>
+                                                            <i class="bi bi-github"></i> &nbsp; 
+                                                            <a href="https://github.com/MorganServer/emergencyprep.git" target="_blank" class="">gmorgan9</a>
+                                                        </p>
+                                                    </li>
+                                                    <li>
+                                                        <p>
+                                                            <i class="bi bi-globe"></i> &nbsp; 
+                                                            <a href="https://emergencyprep.morganserver.com" target="_blank">EmergencyPrep</a>
+                                                        </p>
+                                                    </li>
+                                                    <li>
+                                                        <p>
+                                                            <i class="bi bi-calendar3"></i> &nbsp; 
+                                                            March 7, 2024
+                                                        </p>
+                                                    </li>
+                                                </ul>
+                                                <div class="text-justify">
+                                                    <p>
+                                                        This website serves as a vital resource hub for a church organization, offering essential information and tools for emergency preparedness.
+                                                    </p>
+                                                </div>
+                                                <div class="tags-block">
+                                                    <div class="block-title">
+                                                        <h3>Technology</h3>
+                                                    </div>
+                                                    <ul class="tags">
+                                                        <li><a href="">HTML</a></li>
+                                                        <li><a href="">CSS</a></li>
+                                                        <li><a href="">EmergencyPrep</a></li>
+                                                        <li><a href="">Website</a></li>
+                                                    </ul>
+                                                </div>
+                                                <div class="share-buttons">
+                                                    <div class="block-title">
+                                                        <h3>Share</h3>
+                                                    </div>
+                                                    <div class="btn-group">
+                                                        <a href="https://www.facebook.com/sharer/sharer.php?u=https://lmpixels.com/wp/leven-wp/full-width-dark/project/full-project-2/" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;" class="btn" target="_blank" title="Share on Facebook">
+                                                            <i class="bi bi-facebook"></i>
+                                                        </a>
+                                                        <a href="https://twitter.com/share?url=https://lmpixels.com/wp/leven-wp/full-width-dark/project/full-project-2/" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;" class="btn" target="_blank" title="Share on Twitter">
+                                                            <i class="bi bi-twitter"></i>
+                                                        </a>
+                                                        <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=https://lmpixels.com/wp/leven-wp/full-width-dark/project/full-project-2/" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" class="btn" title="Share on LinkedIn">
+                                                            <i class="bi bi-linkedin"></i>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
