4e97f4ab9e4281a4549ca6736193539525705a87
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 13:59:19 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -104,7 +104,7 @@ logoutUser($conn);
                         <figure class="item standard shuffle-item filtered" data-groups="[&quot;category_all&quot;, &quot;category_detailed&quot;]" style="position: absolute; top: 0px; left: 0px; visibility: visible; transition: transform 450ms ease-out, opacity 450ms ease-out;">
                             <div class="portfolio-item-img">
                                 <!-- <img fetchpriority="high" decoding="async" width="1280" height="853" src="https://lmpixels.com/wp/leven-wp/full-width-dark/wp-content/uploads/sites/5/2019/12/12.jpg" class="attachment-portfolio-image-three-c size-portfolio-image-three-c wp-post-image" alt="Full Project 2" title="" srcset="https://lmpixels.com/wp/leven-wp/full-width-dark/wp-content/uploads/sites/5/2019/12/12.jpg 1280w, https://lmpixels.com/wp/leven-wp/full-width-dark/wp-content/uploads/sites/5/2019/12/12-300x200.jpg 300w, https://lmpixels.com/wp/leven-wp/full-width-dark/wp-content/uploads/sites/5/2019/12/12-1024x682.jpg 1024w, https://lmpixels.com/wp/leven-wp/full-width-dark/wp-content/uploads/sites/5/2019/12/12-768x512.jpg 768w" sizes="(max-width: 768px) 92vw, (max-width: 992px) 450px, (max-width: 1200px) 597px, 25vw"> -->
-                                 <img src="../../assets/images/project-images/emergency-prep" alt="">
+                                 <img fetchpriority="high" decoding="async" width="1280" height="853" src="../../assets/images/project-images/emergency-prep" alt="">
                                 <a href="https://lmpixels.com/wp/leven-wp/full-width-dark/project/full-project-2/" class="ajax-page-load"></a>
                             </div>
                         </figure>
