fe99bb32e05da7a1a8866fab09683955b6ebeb8f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 13:12:43 -0500

    update

--- home/resume/index.php
+++ home/resume/index.php
@@ -21,17 +21,17 @@
         <nav class="d-flex justify-content-between align-items-center" style="padding: 40px 70px 0px 70px;">
             <div class="left">
                 <a href="/home" class="text-white text-decoration-none">
-                    <img src="../../assets/images/logo.png" alt="" style="height: 44px; width: 44px;">
+                    <img src="../assets/images/logo.png" alt="" style="height: 44px; width: 44px;">
                     &nbsp;<span style="font-size: 20px;"><strong>Garrett</strong> Morgan</span>
                 </a>
             </div>
             <div class="right">
                 <ul class="nav">
                     <li class="nav-item">
-                        <a class="nav-link text-secondary" href="/home">About Me</a>
+                        <a class="nav-link text-secondary active" href="/home">About Me</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-secondary active" href="/home/resume">Resume</a>
+                        <a class="nav-link text-secondary" href="/home/resume">Resume</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-secondary" href="/home/projects">Projects</a>
@@ -39,9 +39,22 @@
                     <li class="nav-item">
                         <a class="nav-link text-secondary" href="/home/contact">Contact</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-secondary" href="/login">Login</a>
-                    </li>
+                    <?php if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == 1) { ?>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-secondary" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="bi bi-person-circle"></i>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="margin-left: -50px;">
+                                <a class="dropdown-item" href="/dashboard">Dashboard</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="index.php?logout=1">Logout</a>
+                            </div>
+                        </li>  
+                    <?php } else { ?>
+                        <li class="nav-item">
+                            <a class="nav-link text-secondary" href="/login">Login</a>
+                        </li>
+                    <?php } ?>
                 </ul>
             </div>
         </nav>
