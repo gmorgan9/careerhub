61e6764df8b15c362a2cd934c6937830810ca9b8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 16:55:42 -0500

    update

--- home/projects/test.php
+++ home/projects/test.php
@@ -1,8 +1,42 @@
+<?php
+date_default_timezone_set('America/Denver');
+require_once "../../app/database/connection.php";
+require_once "../../path.php";
+session_start();
+include 'offcanvas.php';
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$files = glob("../../app/functions/*.php");
+foreach ($files as $file) {
+    require_once $file;
+}
+
+logoutUser($conn);
+// if(isLoggedIn() == false) {
+//     header('location:' . BASE_URL . '/login.php');
+// }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>PHP Page with Close Button</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" href="../../assets/css/home.css?v=<?php echo time(); ?>">
+
+
+    <title>Projects - MorganServer Career Hub</title>
+
+    
 </head>
 <body>
 
