a45f83ff718cd50c26a6e72babce775755fb5547
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 18:08:21 -0500

    update

--- console/admin/add-project/index.php
+++ console/admin/add-project/index.php
@@ -107,7 +107,7 @@ foreach ($files as $file) {
                     </div>
 
 
-                    <footer class="site-footer">
+                    <footer class="site-footer" style="margin: 120px !important;">
                         <div class="footer-socials">
                             <ul class="footer-social-links">
                                 <li><a href="#" target="_blank">Twitter</a></li>
