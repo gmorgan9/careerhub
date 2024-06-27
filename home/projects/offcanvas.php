154edb25a0ea964c7fab9c37701f2ccbf24c8bd4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 16:54:43 -0500

    update

--- home/projects/offcanvas.php
+++ home/projects/offcanvas.php
@@ -23,7 +23,7 @@
                                                 <div class="block-title">
                                                     <h3>Description</h3>
                                                 </div>
-                                                <a href="https://careerhub.morganserver.com/projects/test.php"></a>
+                                                <a href="https://careerhub.morganserver.com/projects/test.php">test</a>
                                                 <ul class="project-general-info">
                                                     <li>
                                                         <p>
