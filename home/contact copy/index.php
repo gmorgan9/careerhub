d0ac660c4ad2d424b7d893bb1eaa055e87f7ecdc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 13:29:22 -0500

    update

---
+++ home/contact copy/index.php
@@ -0,0 +1,116 @@
+<?php
+date_default_timezone_set('America/Denver');
+require_once "../../app/database/connection.php";
+require_once "../../path.php";
+session_start();
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$files = glob("../../app/functions/*.php");
+foreach ($files as $file) {
+    require_once $file;
+}
+
+logoutUser($conn);
+// if(isLoggedIn() == false) {
+//     header('location:' . BASE_URL . '/login.php');
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" href="../../assets/css/home.css?v=<?php echo time(); ?>">
+
+
+    <title>Projects - MorganServer Career Hub</title>
+
+    
+</head>
+<body style="background-color: rgb(34,34,34);">
+    
+    <!-- Navbar -->
+        <nav class="d-flex justify-content-between align-items-center" style="padding: 40px 70px 0px 70px;">
+            <div class="left">
+                <a href="/home" class="text-white text-decoration-none">
+                    <img src="../../assets/images/logo.png" alt="" style="height: 44px; width: 44px;">
+                    &nbsp;<span style="font-size: 20px;"><strong>Garrett</strong> Morgan</span>
+                </a>
+            </div>
+            <div class="right">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary" href="/home">About Me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary" href="/home/resume">Resume</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary active" href="/home/projects">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary" href="/home/contact">Contact</a>
+                    </li>
+                    <?php if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == 1) { ?>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-secondary" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="bi bi-person-circle"></i>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="margin-left: -50px;">
+                                <a class="dropdown-item" href="/dashboard">Dashboard</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="index.php?logout=1">Logout</a>
+                            </div>
+                        </li>  
+                    <?php } else { ?>
+                        <li class="nav-item">
+                            <a class="nav-link text-secondary" href="/login">Login</a>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </nav>
+    <!-- End Navbar -->
+
+    <div class="container w-100">
+        <div class="page_title">
+            <h2 class="text-white title">
+                Projects
+            </h2>
+        </div>
+        
+        <div class="content text-white" style="max-width: 1320px; margin: 0 auto;">
+            
+
+        </div>
+
+        
+        <footer class="site-footer">
+            <div class="footer-socials">
+                <ul class="footer-social-links">
+                    <li><a href="#" target="_blank">Twitter</a></li>
+                    <li><a href="#" target="_blank">Facebook</a></li>
+                    <li><a href="#" target="_blank">Instagram</a></li>
+                </ul>
+
+            </div>
+            <div class="footer-copyright">
+                <p>© 2024 All rights reserved.</p>
+            </div>
+        </footer>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
