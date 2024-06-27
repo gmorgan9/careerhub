b503d2efdc96bfb3ab6f554fcc4249168113363f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 15:14:18 -0500

    update

--- home/projects/test.php
+++ home/projects/test.php
@@ -1,11 +1,2 @@
 testing 
 
-<button onclick="closeOffcanvas()">Close</button>
-
-
-<script>
-    function closeOffcanvas() {
-    var offcanvas = document.getElementById('offcanvas');
-    offcanvas.classList.remove('open');
-}
-</script>
