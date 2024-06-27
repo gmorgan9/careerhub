b4a097c64deb704c9d2fef1b6e7325dd737533d5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 07:20:31 -0500

    update

--- home/contact/index.php
+++ home/contact/index.php
@@ -166,9 +166,8 @@
                         <form action="">
                             <!-- ------ -->
                             <div class="mb-3">
-                              <label for="exampleInputEmail1" class="form-label">Email address</label>
-                              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                              <!-- <label for="exampleInputEmail1" class="form-label">Email address</label> -->
+                              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email Address" style="background-color: transparent;">
                             </div>
                             <!-- ------ -->
                             <button class="form-btn" style="">Send message</button>
