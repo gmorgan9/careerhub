f0a230e528c3841b661f4e47feeb9fd374cd0ea0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-26 23:05:56 -0500

    update

--- home/index.php
+++ home/index.php
@@ -39,7 +39,7 @@
         </div>
     </nav>
 
-    <nav class="navbar navbar-expand-lg d-flex justify-content-between align-items-center">
+    <nav class="navbar navbar-expand-lg bg-dark d-flex justify-content-between align-items-center">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
