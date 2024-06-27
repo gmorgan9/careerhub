1b76c558fde92cdc4512c9155ec3ad3284056d29
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 14:56:23 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -119,7 +119,9 @@ logoutUser($conn);
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-fullscreen">
                 <div class="modal-content custom-modal-bg">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div class="close-btn">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
                     <div class="modal-body">
                         <p>
                             Testing here
