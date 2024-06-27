0f0f276c2a60c3abeb728fb6a0a9501c59feece6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 12:46:26 -0500

    update

--- login/index.php
+++ login/index.php
@@ -32,7 +32,7 @@ if(isLoggedIn() == true) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
   <!-- Custom CSS -->
-  <link rel="stylesheet" href="assets/css/loginpage.css?v=<?php echo time(); ?>">
+  <link rel="stylesheet" href="../assets/css/loginpage.css?v=<?php echo time(); ?>">
 
   <!-- Page Title -->
   <title>Sign In | WMS</title>
@@ -66,7 +66,7 @@ if(isLoggedIn() == true) {
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="assets/js/loginpage.js"></script>
+    <script src="../assets/js/loginpage.js"></script>
      
 </body>
 
