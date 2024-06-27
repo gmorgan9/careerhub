4d59185836604e251704946786384a51252b7f88
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 11:21:23 -0500

    update

--- home/resume/index.php
+++ home/resume/index.php
@@ -56,79 +56,87 @@
         
         <div class="content text-white" style="max-width: 1320px; margin: 0 auto;">
             <div class="row">
-                <div class="col-xs-12 col-sm-6 ">
-                    <div class="block-title">
-                        <h2>Education</h2>
+                <div class="block-title">
+                    <h2>Experience</h2>
+                </div>
+            </div>
+            <div class="row">     
+                <div class="timeline clearfix">
+                    <div class="timeline-item">
+                        <h5 class="item-period ">Jul 2023 - Current</h5>
+                        <span class="item-company">AARC-360</span>
+                        <h4 class="item-title">Staff Audit Consultant</h4>
+                        <div class="text">
+                            <ul>
+                                <li>
+                                    Conducted over 15 comprehensive audits, specializing in IT Security Audits and Assessments including SSAE 21 (SOC 1 and SOC 2), PCI DSS Assessments, and HITRUST certifications.
+                                </li>
+                                <li>
+                                    Collaborated with clients to identify and implement improvements in each audit, resulting in a 20% increase in their overall security posture.
+                                </li>
+                                <li>
+                                    Delivered comprehensive reports to client leadership, outlining findings, risks, and strategic recommendations, resulting in a 15% increase in informed decision-making efficiency.
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="timeline clearfix">
-                        <div class="timeline-item">
-                            <h5 class="item-period ">2023</h5>
-                            <span class="item-company">Brigham Young University</span>
-                            <h4 class="item-title">Bachelor of Science in Cybersecurity</h4>
-                            <div class="text">
-        	                    Duis posuere, quam non imperdiet egestas, eros enim mattis mauris, in posuere lacus arcu quis felis. Etiam interdum erat non enim venenatis fermentum.
-                            </div>
+                    <div class="timeline-item">
+                        <h5 class="item-period ">Jan 2023 - July 2023</h5>
+                        <span class="item-company">Arctic Wolf</span>
+                        <h4 class="item-title">Triage Security Analyst Intern</h4>
+                        <div class="text">
+                            <ul>
+                                <li>
+                                    Triaged 500+ customer requests daily, applied playbook driven solutions, and initiated incident investigations.
+                                </li>
+                                <li>
+                                    Developed new run books resulting in a 20% increase in team efficiency through streamlined processes and enhanced knowledge management.
+                                </li>
+                                <li>
+                                    Troubleshot and resolved over 100 issues with sensors and scanners, achieving a 97% success rate in maintaining operational health and minimizing downtime..
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="timeline-item">
+                        <h5 class="item-period ">Jan 2023 - July 2023</h5>
+                        <span class="item-company">Brigham Young University Office of Information Technology</span>
+                        <h4 class="item-title">Student Systems Engineer</h4>
+                        <div class="text">
+                            <ul>
+                                <li>
+                                    Managed and optimized over 10 ESXi hypervisors, utilizing vCenter technology to oversee and enhance the performance of 200+ virtual machines (VMs), resulting in a 20% reduction in downtime and a 15% improvement in overall system efficiency.
+                                </li>
+                                <li>
+                                    Diagnosed and resolved technical challenges pertaining to Dell MX chassis, blades, and network switches.
+                                </li>
+                                <li>
+                                Documented 15+ processes and procedures for future reference, enhancing team efficiency by 10% and facilitating knowledge sharing across departments.
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
+                <!-- <div class="col-xs-12 col-sm-6 ">
+                    
+                </div>
                 <div class="col-xs-12 col-sm-6 ">
-                    <div class="block-title">
-                        <h2>Experience</h2>
-                    </div>
-                    <div class="timeline clearfix">
-                        <div class="timeline-item">
-                            <h5 class="item-period ">Jul 2023 - Current</h5>
-                            <span class="item-company">AARC-360</span>
-                            <h4 class="item-title">Staff Audit Consultant</h4>
-                            <div class="text">
-                                <ul>
-                                    <li>
-                                        Conducted over 15 comprehensive audits, specializing in IT Security Audits and Assessments including SSAE 21 (SOC 1 and SOC 2), PCI DSS Assessments, and HITRUST certifications.
-                                    </li>
-                                    <li>
-                                        Collaborated with clients to identify and implement improvements in each audit, resulting in a 20% increase in their overall security posture.
-                                    </li>
-                                    <li>
-                                        Delivered comprehensive reports to client leadership, outlining findings, risks, and strategic recommendations, resulting in a 15% increase in informed decision-making efficiency.
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="timeline-item">
-                            <h5 class="item-period ">Jan 2023 - July 2023</h5>
-                            <span class="item-company">Arctic Wolf</span>
-                            <h4 class="item-title">Triage Security Analyst Intern</h4>
-                            <div class="text">
-                                <ul>
-                                    <li>
-                                        Triaged 500+ customer requests daily, applied playbook driven solutions, and initiated incident investigations.
-                                    </li>
-                                    <li>
-                                        Developed new run books resulting in a 20% increase in team efficiency through streamlined processes and enhanced knowledge management.
-                                    </li>
-                                    <li>
-                                        Troubleshot and resolved over 100 issues with sensors and scanners, achieving a 97% success rate in maintaining operational health and minimizing downtime..
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="timeline-item">
-                            <h5 class="item-period ">Jan 2023 - July 2023</h5>
-                            <span class="item-company">Brigham Young University Office of Information Technology</span>
-                            <h4 class="item-title">Student Systems Engineer</h4>
-                            <div class="text">
-                                <ul>
-                                    <li>
-                                        Managed and optimized over 10 ESXi hypervisors, utilizing vCenter technology to oversee and enhance the performance of 200+ virtual machines (VMs), resulting in a 20% reduction in downtime and a 15% improvement in overall system efficiency.
-                                    </li>
-                                    <li>
-                                        Diagnosed and resolved technical challenges pertaining to Dell MX chassis, blades, and network switches.
-                                    </li>
-                                    <li>
-                                    Documented 15+ processes and procedures for future reference, enhancing team efficiency by 10% and facilitating knowledge sharing across departments.
-                                    </li>
-                                </ul>
-                            </div>
+                    
+                </div> -->
+            </div>
+            <div class="row">
+                <div class="block-title">
+                    <h2>Education</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="timeline clearfix">
+                    <div class="timeline-item">
+                        <h5 class="item-period ">2023</h5>
+                        <span class="item-company">Brigham Young University</span>
+                        <h4 class="item-title">Bachelor of Science in Cybersecurity</h4>
+                        <div class="text">
+        	                Duis posuere, quam non imperdiet egestas, eros enim mattis mauris, in posuere lacus arcu quis felis. Etiam interdum erat non enim venenatis fermentum.
                         </div>
                     </div>
                 </div>
