01edb171923a24632516084545a9fd8d212c578e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 06:52:12 -0500

    update

--- home/contact/index.php
+++ home/contact/index.php
@@ -6,6 +6,8 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
 
     <title>Contact - MorganServer Career Hub</title>
 
@@ -36,6 +38,18 @@
         }
         .title {
             font-size: 44px;
+        }
+        .info-icon {
+            position: relative;
+            display: table-cell;
+            padding: 0 10px 5px 0;
+            width: 44px;
+        }
+        .info-text {
+            position: relative;
+            display: table-cell;
+            padding: 0 0 0 15px;
+            vertical-align: middle;
         }
     </style>
 </head>
@@ -80,7 +94,14 @@
         <div class="content text-white" style="max-width: 1320px; margin: 0 auto;">
             <div class="row">
                 <div class=" col-xs-12 col-sm-4 ">
-                    text
+                    <div class="info-block">
+                        <div class="info-icon">
+                            <i class="bi bi-envelope-fill"></i>
+                        </div>
+                        <div class="info-text">
+                            <h4>McKinney, TX</h4>
+                        </div>
+                    </div>
                 </div>
                 <div class=" col-xs-12 col-sm-8 ">
                     text
