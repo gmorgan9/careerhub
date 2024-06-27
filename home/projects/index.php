7d0ed001489d2a4ab29e3fe21da456717dfd2481
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 14:48:15 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -119,7 +119,7 @@ logoutUser($conn);
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-fullscreen">
                 <div class="modal-content custom-modal-bg">
-                    <div class="modal-body d-flex justify-content-center">
+                    <div class="modal-body d-flex justify-content-center flex-column">
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         <p>
                             Testing here
