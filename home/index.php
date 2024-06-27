e50f966ecdaec5994884633c8c67558d7ebe38de
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-26 22:21:21 -0500

    update

---
+++ home/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - MorganServer Career Hub</title>
+</head>
+<body>
+    <h1>
+        MorganServer Career Hub
+    </h1>
+</body>
+</html>
