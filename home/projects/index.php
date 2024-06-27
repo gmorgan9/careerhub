d0c4d3bd509b04cbda2fd5c20a976e9e499ffce1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 14:15:07 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -117,21 +117,23 @@ logoutUser($conn);
         </div> <!-- end -->
 
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-xl bg-dark" style="background-color: #444;">
-                <div class="modal-content">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content custom-modal-bg">
                     <div class="modal-header">
                         <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                <div class="modal-body">
-                    ...
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-             </div>
+            </div>
         </div>
+
 
         
         <footer class="site-footer">
