f1b78c63c5799bacbd86a387bf758666b9423d79
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-26 22:36:28 -0500

    update

--- home/index.php
+++ home/index.php
@@ -7,11 +7,11 @@
 </head>
 <body>
     
-    <nav>
-        <div class="left" style="display: flex; justify-content: left;">
+    <nav style="display: flex;">
+        <div class="left" style="justify-content: left;">
             test
         </div>
-        <div class="right" style="display: flex; justify-content: right;">
+        <div class="right" style="justify-content: right;">
             test2
         </div>
     </nav>
