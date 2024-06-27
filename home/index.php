c7df6694cc3f3dcb5ffbe96a9bafa00675fb7a68
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-26 23:08:16 -0500

    update

--- home/index.php
+++ home/index.php
@@ -42,7 +42,7 @@
     <nav class="navbar d-flex justify-content-between align-items-center">
         <div class="container-fluid">
             <div class="left">
-                <a href="home/" class="text-white text-decoration-none">
+                <a href="/" class="text-white text-decoration-none">
                     <img src="../assets/images/logo.png" alt="" style="height: 44px; width: 44px;">
                 &nbsp;<span style="font-size: 20px;"><strong>Garrett</strong> Morgan</span>
                 </a>
