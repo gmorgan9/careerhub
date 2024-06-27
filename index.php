cd7dc0a881bb3e6776866834c147fd1c46de4a34
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-26 22:21:46 -0500

    update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Redirect</title>
     <?php
-    header("Location: home/index.php");
+    header("Location: home/");
     exit();
     ?>
 </head>
