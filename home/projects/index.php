a0ebd88e870492dca74c3451d0591ec3b869fbc4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 15:25:28 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -118,7 +118,7 @@ logoutUser($conn);
     <h5 class="offcanvas-title" id="offcanvasTopLabel">Offcanvas top</h5>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
-  <div class="offcanvas-body">
+  <div class="offcanvas-body large">
     ...
   </div>
 </div>
