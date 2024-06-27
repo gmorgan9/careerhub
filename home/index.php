f5f94fe957770b33f51d35ab5667454ffc275e5b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 13:05:01 -0500

    update

--- home/index.php
+++ home/index.php
@@ -12,7 +12,7 @@ foreach ($files as $file) {
     require_once $file;
 }
 
-// logoutUser($conn);
+logoutUser($conn);
 // if(isLoggedIn() == false) {
 //     header('location:' . BASE_URL . '/login.php');
 // }
