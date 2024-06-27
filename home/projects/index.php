79b396048071de3a484fbc58195ecf6084c776bb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 15:28:14 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -111,15 +111,23 @@ logoutUser($conn);
                             
                         </figure>
                     </div>
-                    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Toggle top offcanvas</button>
 
+                    
+                    <!-- Button to toggle the offcanvas -->
+<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
+  Toggle top offcanvas
+</button>
+
+<!-- Offcanvas container -->
 <div class="offcanvas offcanvas-lg" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
   <div class="offcanvas-header">
     <h5 class="offcanvas-title" id="offcanvasTopLabel">Offcanvas top</h5>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
-    ...
+    <p>Your PHP Page Content</p>
+    <!-- Close button to close the offcanvas -->
+    <button class="btn btn-secondary" onclick="closeOffcanvas()">Close</button>
   </div>
 </div>
 
