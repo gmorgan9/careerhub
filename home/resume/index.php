a107346254f6165311bf1e83929879218228a7b1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 08:13:23 -0500

    update

---
+++ home/resume/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" href="../../assets/css/home.css?v=<?php echo time(); ?>">
+
+
+    <title>Resume - MorganServer Career Hub</title>
+
+    
+</head>
+<body style="background-color: rgb(34,34,34);">
+    
+    <!-- Navbar -->
+        <nav class="d-flex justify-content-between align-items-center" style="padding: 40px 70px 0px 70px;">
+            <div class="left">
+                <a href="/home" class="text-white text-decoration-none">
+                    <img src="../../assets/images/logo.png" alt="" style="height: 44px; width: 44px;">
+                    &nbsp;<span style="font-size: 20px;"><strong>Garrett</strong> Morgan</span>
+                </a>
+            </div>
+            <div class="right">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary" href="/home">About Me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary active" href="/home/resume">Resume</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary" href="/home/projects">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary" href="/home/contact">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary" href="/login">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    <!-- End Navbar -->
+
+    <div class="container w-100">
+        <div class="page_title">
+            <h2 class="text-white title">
+                Résumé
+            </h2>
+        </div>
+        
+        <div class="content text-white" style="max-width: 1320px; margin: 0 auto;">
+           
+        </div>
+
+        <footer class="site-footer">
+            <div class="footer-socials">
+                <ul class="footer-social-links">
+                    <li><a href="#" target="_blank">Twitter</a></li>
+                    <li><a href="#" target="_blank">Facebook</a></li>
+                    <li><a href="#" target="_blank">Instagram</a></li>
+                </ul>
+
+            </div>
+            <div class="footer-copyright">
+                <p>© 2024 All rights reserved.</p>
+            </div>
+        </footer>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
