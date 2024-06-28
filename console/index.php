3069ec4d83c4e9c3cf84e5bfb3906671ef8d4ce9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 22:27:42 -0500

    update

--- console/index.php
+++ console/index.php
@@ -131,7 +131,7 @@ foreach ($files as $file) {
 
         <!-- Open/Awaiting -->
             
-            <div class="card top-card me-2" style="min-width: 18rem; max-width: 18rem; background-color: #333333;">
+            <div class="card top-card me-2" style="min-width: 18rem; max-width: 18rem; background-color: #333333; color: white;">
                 <a class="text-decoration-none text-black stretched-link" href="open-app.php">
                     <div class="card-body p-0">
                         <div class="left float-start" style="background-color: lightgreen; height: 100%; border-top-left-radius: 0.325rem; border-bottom-left-radius: 0.325rem;">
