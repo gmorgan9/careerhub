50165aaa2dbf761cb04f3970c188b8f1725b03eb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-28 11:03:05 -0500

    update

--- console/jobs/open-jobs/index.php
+++ console/jobs/open-jobs/index.php
@@ -71,7 +71,7 @@ $result = mysqli_query($conn, $sql);
         <h2 class="text-white title">Open Jobs</h2>
     </div>
 
-    <div class="content text-white" style="margin-top: 55px;">
+    <!-- <div class="content text-white" style="margin-top: 55px;"> -->
         <table class="table text-white">
             <thead>
                 <tr>
@@ -148,7 +148,7 @@ $result = mysqli_query($conn, $sql);
             echo '</ul>';
         }
         ?>
-    </div>
+    <!-- </div> -->
     
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
