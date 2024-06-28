7e8e2cc180e7cf643c435ccaf844ac11d2fb9c01
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 22:49:00 -0500

    update

--- console/jobs/open-jobs/index.php
+++ console/jobs/open-jobs/index.php
@@ -70,7 +70,12 @@ foreach ($files as $file) {
 <!-- main-container -->
 
 <div class="content text-white" style="max-width: 1320px; margin: 0 auto; margin-top: 55px;">
-<h1 class="text-center"><strong>Open Applications</strong></h1><br>
+<!-- <h1 class="text-center"><strong>Open Applications</strong></h1><br> -->
+        <div class="page_title">
+            <h2 class="text-white title">
+                Open Jobs
+            </h2>
+        </div>
 
     <div class="container-fluid main">
 
