56996662732695415d69feb144e2da22d7db7d0d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-28 12:58:41 -0500

    update

--- console/index.php
+++ console/index.php
@@ -22,7 +22,7 @@ foreach ($files as $file) {
     <link rel="icon" type="image/x-icon" href="../assets/images/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="../assets/css/home.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/home.css?v=<?php echo time(); ?>">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
