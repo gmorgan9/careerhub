21b6b9e3f7982c1de9c8010870a9146555980558
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-28 07:12:47 -0500

    uipdate

--- console/jobs/total-jobs/index.php
+++ console/jobs/total-jobs/index.php
@@ -74,14 +74,8 @@ foreach ($files as $file) {
                 Total Jobs
             </h2>
         </div>
-        
+
     <div class="content text-white" style="max-width: 1320px; margin: 0 auto; margin-top: 55px;">
-<!-- <h1 class="text-center"><strong>Open Applications</strong></h1><br> -->
-        <div class="page_title">
-            <h2 class="text-white title">
-                Total Jobs
-            </h2>
-        </div>
 
     <div class="container-fluid main">
 
