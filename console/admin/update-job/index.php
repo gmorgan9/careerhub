279bc6f6c9a311bb7e557bf643b2edac47a0d625
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-28 12:04:39 -0500

    update

--- console/admin/update-job/index.php
+++ console/admin/update-job/index.php
@@ -9,7 +9,6 @@ foreach ($files as $file) {
     require_once $file;
 }
 
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,10 +27,6 @@ foreach ($files as $file) {
     <script src="https://cdn.tiny.cloud/1/7kainuaawjddfzf3pj7t2fm3qdjgq5smjfjtsw3l4kqfd1h4/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
     <title>Update Job - MorganServer Career Hub</title>
-
-    <style>
-        
-    </style>
     
 </head>
 <body style="background-color: rgb(34,34,34);">
@@ -39,20 +34,16 @@ foreach ($files as $file) {
 
     <?php include(ROOT_PATH . "/app/database/includes/console-header.php"); ?>
 
-
- <!-- main-container -->
-
- <div class="container w-100">
     <div class="page_title">
-            <h2 class="text-white title">
-                Update Job
-            </h2>
-        </div>
-        
-    <div class="content text-white" style="max-width: 1320px; margin: 0 auto; margin-top: 55px;">
-<br>
+        <h2 class="text-white title">
+            Update Job
+        </h2>
+    </div>
 
-    <?php
+    <div class="container-fluid">   
+        <div class="content text-white" style="max-width: 1320px; margin: 0 auto; margin-top: 55px;">
+
+            <?php
             $id = $_GET['updateid'];
             $sql = "SELECT * FROM jobs WHERE job_id=$id";
             $result = mysqli_query($conn, $sql);
@@ -77,124 +68,111 @@ foreach ($files as $file) {
                     $notes          = $row['notes'];
                     $watchlist      = $row['watchlist'];
                     $interview_set  = $row['interview_set'];
-    ?>
+                ?>
 
-    <form method="POST" action="">
+                <form method="POST" action="">
 
-        <input type="hidden" class="form-input" id="job_id" name="job_id" value="<?php echo $job_id; ?>">
+                    <input type="hidden" class="form-input" id="job_id" name="job_id" value="<?php echo $job_id; ?>">
 
-        <div class="row mb-3">
-            <div class="col">
-                <label for="job_title" class="form-label">Job Title</label>
-                <input type="text" class="form-input" id="job_title" name="job_title" value="<?php echo $job_title; ?>">
-            </div>
-            <div class="col">
-                <label for="job_link" class="form-label">Job Link</label>
-                <input type="text" class="form-input" id="job_link" name="job_link" value="<?php echo $job_link; ?>">
-            </div>
-        </div>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="job_title" class="form-label">Job Title</label>
+                            <input type="text" class="form-input" id="job_title" name="job_title" value="<?php echo $job_title; ?>">
+                        </div>
+                        <div class="col">
+                            <label for="job_link" class="form-label">Job Link</label>
+                            <input type="text" class="form-input" id="job_link" name="job_link" value="<?php echo $job_link; ?>">
+                        </div>
+                    </div>
 
-        <div class="row mb-3">
-            <div class="col">
-                <label for="company" class="form-label">Company</label>
-                <input type="text" class="form-input" id="company" name="company" value="<?php echo $comapny; ?>">
-            </div>
-            <div class="col">
-                <label for="location" class="form-label">Location</label>
-                <input type="text" class="form-input" id="location" name="location" value="<?php echo $location; ?>">
-            </div>
-        </div>
-
-
-        <div class="row mb-3">
-            <div class="col">
-                <label for="pay" class="form-label">Pay</label>
-                <input type="text" class="form-input" id="pay" name="pay" value="<?php echo $pay; ?>">
-            </div>
-            <div class="col">
-                <label for="bonus_pay" class="form-label">Bonus Pay  <span class="text-muted" style="font-size: 10px;">Optional</span></label>
-                <input type="text" class="form-input" id="bonus_pay" name="bonus_pay" value="<?php echo $bonus_pay; ?>">
-            </div>
-            <div class="col">
-                <label class="form-label" for="status">Status</label>
-                <select class="form-input" name="status">
-                    <option value="<?php echo $status; ?>"><?php echo $status; ?></option>
-                    <option value="Applied">Applied</option>
-                    <option value="Interviewed">Interviewed</option>
-                    <option value="Offered">Offered</option>
-                    <option value="Rejected">Rejected</option>
-                </select>
-            </div>
-            <div class="col">
-                <label class="form-label" for="job_type">Job Type</label>
-                <select class="form-input" name="job_type">
-                    <option value="<?php echo $job_type; ?>"><?php echo $job_type; ?></option>
-                    <option value="Full Time">Full Time</option>
-                    <option value="Part Time">Part Time</option>
-                    <option value="Contract">Contract</option>
-                    <option value="Internship">Internship</option>
-                    <option value="Temporary">Temporary</option>
-                </select>
-            </div>
-        </div>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="company" class="form-label">Company</label>
+                            <input type="text" class="form-input" id="company" name="company" value="<?php echo $comapny; ?>">
+                        </div>
+                        <div class="col">
+                            <label for="location" class="form-label">Location</label>
+                            <input type="text" class="form-input" id="location" name="location" value="<?php echo $location; ?>">
+                        </div>
+                    </div>
 
 
-        <div class="row mb-3">
-            <div class="col">
-                <label class="form-label" for="notes">Notes</label>
-                <textarea class="form-input" name="notes" rows="5"><?php echo $notes; ?></textarea>
-            </div>
-        </div>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="pay" class="form-label">Pay</label>
+                            <input type="text" class="form-input" id="pay" name="pay" value="<?php echo $pay; ?>">
+                        </div>
+                        <div class="col">
+                            <label for="bonus_pay" class="form-label">Bonus Pay  <span class="text-muted" style="font-size: 10px;">Optional</span></label>
+                            <input type="text" class="form-input" id="bonus_pay" name="bonus_pay" value="<?php echo $bonus_pay; ?>">
+                        </div>
+                        <div class="col">
+                            <label class="form-label" for="status">Status</label>
+                            <select class="form-input" name="status">
+                                <option value="<?php echo $status; ?>"><?php echo $status; ?></option>
+                                <option value="Applied">Applied</option>
+                                <option value="Interviewed">Interviewed</option>
+                                <option value="Offered">Offered</option>
+                                <option value="Rejected">Rejected</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label class="form-label" for="job_type">Job Type</label>
+                            <select class="form-input" name="job_type">
+                                <option value="<?php echo $job_type; ?>"><?php echo $job_type; ?></option>
+                                <option value="Full Time">Full Time</option>
+                                <option value="Part Time">Part Time</option>
+                                <option value="Contract">Contract</option>
+                                <option value="Internship">Internship</option>
+                                <option value="Temporary">Temporary</option>
+                            </select>
+                        </div>
+                    </div>
 
-        
 
-        <div class="row mb-3 ps-3">
-            <div class="form-check">
-                <?php if($watchlist == 1) { ?>
-                    <input type="checkbox" class="form-check-input" id="watchlist" name="watchlist" value="1" checked>
-                    <label class="form-check-label" for="watchlist">Add to Watchlist</label>
-                <?php } else { ?>
-                    <input type="checkbox" class="form-check-input" id="watchlist" name="watchlist" value="0">
-                    <label class="form-check-label" for="watchlist">Add to Watchlist</label>
-                <?php } ?>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label class="form-label" for="notes">Notes</label>
+                            <textarea class="form-input" name="notes" rows="5"><?php echo $notes; ?></textarea>
+                        </div>
+                    </div>
 
-            </div>
-            <div class="form-check">
-                <?php if($interview_set == 1) { ?>
-                    <input type="checkbox" class="form-check-input" id="interview_set" name="interview_set" value="1" checked>
-                    <label class="form-check-label" for="interview_set">Interview Set</label>
-                <?php } else { ?>
-                    <input type="checkbox" class="form-check-input" id="interview_set" name="interview_set" value="0">
-                    <label class="form-check-label" for="interview_set">Interview Set</label>
-                <?php } ?>
-            </div>
-        </div>
+                    
 
+                    <div class="row mb-3 ps-3">
+                        <div class="form-check">
+                            <?php if($watchlist == 1) { ?>
+                                <input type="checkbox" class="form-check-input" id="watchlist" name="watchlist" value="1" checked>
+                                <label class="form-check-label" for="watchlist">Add to Watchlist</label>
+                            <?php } else { ?>
+                                <input type="checkbox" class="form-check-input" id="watchlist" name="watchlist" value="0">
+                                <label class="form-check-label" for="watchlist">Add to Watchlist</label>
+                            <?php } ?>
 
-    
-        <button type="submit" name="update-full" class="form-btn">Update Job</button>
-        <div class="pb-4"></div>
-    </form>
+                        </div>
+                        <div class="form-check">
+                            <?php if($interview_set == 1) { ?>
+                                <input type="checkbox" class="form-check-input" id="interview_set" name="interview_set" value="1" checked>
+                                <label class="form-check-label" for="interview_set">Interview Set</label>
+                            <?php } else { ?>
+                                <input type="checkbox" class="form-check-input" id="interview_set" name="interview_set" value="0">
+                                <label class="form-check-label" for="interview_set">Interview Set</label>
+                            <?php } ?>
+                        </div>
+                    </div>
 
-    <?php
 
                 
-            }
-        }
+                    <button type="submit" name="update-full" class="form-btn">Update Job</button>
+                    <div class="pb-4"></div>
+                </form>
 
-    ?>
+                <?php } } ?>
 
+        </div>
+    </div>
 
-
-</div>
-<!-- END main-container -->
-
-<br><br><br>
-
-
-
-
-
+    <div class="mt-5"></div>
 
     <script>
         tinymce.init({
