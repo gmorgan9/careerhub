89bb2fe542d6ae25b84c9e456a65ac74399676ff
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-28 11:10:30 -0500

    update

--- console/jobs/open-jobs/index.php
+++ console/jobs/open-jobs/index.php
@@ -72,7 +72,7 @@ $result = mysqli_query($conn, $sql);
         </div>
 
         <!-- <div class="content text-white" style="margin-top: 55px;"> -->
-        <table class="table text-white w-75" style="margin: auto;">
+        <table class="table text-white w-75" style="margin-left: auto;">
             <thead>
                 <tr>
                     <th scope="col">#</th>
