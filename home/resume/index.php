1a74e2748bdbde3499045c7bb784e457dc887553
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-28 12:26:30 -0500

    update

--- home/resume/index.php
+++ home/resume/index.php
@@ -260,21 +260,8 @@ foreach ($files as $file) {
 
         
     </div>
-    <footer class="site-footer w-100">
-            <div class="footer-socials">
-                <ul class="footer-social-links">
-                    <li><a href="#" target="_blank">Twitter</a></li>
-                    <li><a href="#" target="_blank">Facebook</a></li>
-                    <li><a href="#" target="_blank">Instagram</a></li>
-                </ul>
 
-            </div>
-            <div class="footer-copyright">
-                <p>© 2024 All rights reserved.</p>
-            </div>
-        </footer>
-
-
+    <?php include(ROOT_PATH . "/app/database/includes/site-footer.php"); ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
