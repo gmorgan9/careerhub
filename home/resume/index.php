0120c963630515bebb9e3ae8f5170e2f97ff2078
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-28 12:16:41 -0500

    update

--- home/resume/index.php
+++ home/resume/index.php
@@ -258,7 +258,7 @@ foreach ($files as $file) {
             </div>
         </div>
 
-        <footer class=" container-fluid site-footer">
+        <footer class="site-footer">
             <div class="footer-socials">
                 <ul class="footer-social-links">
                     <li><a href="#" target="_blank">Twitter</a></li>
