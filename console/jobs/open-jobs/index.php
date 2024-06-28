fa3a93c76bddb2bc92f6492b924846058e9fe908
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 22:47:54 -0500

    update

--- console/jobs/open-jobs/index.php
+++ console/jobs/open-jobs/index.php
@@ -18,7 +18,7 @@ foreach ($files as $file) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../assets/css/main.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../../../assets/css/home.css?v=<?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
 
