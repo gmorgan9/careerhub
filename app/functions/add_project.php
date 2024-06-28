3860b846af346709a93f84b68d6a78bf88651070
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-27 20:41:27 -0500

    update

--- app/functions/add_project.php
+++ app/functions/add_project.php
@@ -6,7 +6,8 @@
 		if(isset($_POST['project_name'])) { $project_name = mysqli_real_escape_string($conn, $_POST['project_name']); } else { $project_name = ""; }
 		if(isset($_POST['project_description'])) { $project_description = mysqli_real_escape_string($conn, $_POST['project_description']); } else { $project_description = ""; }
 		if(isset($_POST['project_description'])) { $project_description = mysqli_real_escape_string($conn, $_POST['project_description']); } else { $project_description = ""; }
-		if(isset($_POST['project_github'])) { $project_github = mysqli_real_escape_string($conn, $_POST['project_github']); } else { $project_github = ""; }
+		if(isset($_POST['project_github_link'])) { $project_github_link = mysqli_real_escape_string($conn, $_POST['project_github_link']); } else { $project_github_link = ""; }
+		if(isset($_POST['project_github_user'])) { $project_github_user = mysqli_real_escape_string($conn, $_POST['project_github_user']); } else { $project_github_user = ""; }
 		if(isset($_POST['project_url'])) { $project_url = mysqli_real_escape_string($conn, $_POST['project_url']); } else { $project_url = ""; }
 		if(isset($_POST['project_release'])) { $project_release = mysqli_real_escape_string($conn, $_POST['project_release']); } else { $project_release = ""; }
 		if(isset($_POST['project_tech'])) { $project_tech = mysqli_real_escape_string($conn, $_POST['project_tech']); } else { $project_tech = ""; }
@@ -19,7 +20,7 @@
 		if (mysqli_num_rows($result) > 0) {
 			$error[] = 'The project already exists!';
 		} else {
-			$insert = "INSERT INTO projects (idno, project_name, project_description, project_github, project_url, project_release, project_tech, project_content) VALUES ('$idno', NULLIF('$project_name',''), NULLIF('$project_description',''), NULLIF('$project_github',''), NULLIF('$project_url',''), NULLIF('$project_release',''), NULLIF('$project_tech',''), NULLIF('$project_content',''))";
+			$insert = "INSERT INTO projects (idno, project_name, project_description, project_github_link, project_github_user, project_url, project_release, project_tech, project_content) VALUES ('$idno', NULLIF('$project_name',''), NULLIF('$project_description',''), NULLIF('$project_github_link',''), NULLIF('$project_github_user',''), NULLIF('$project_url',''), NULLIF('$project_release',''), NULLIF('$project_tech',''), NULLIF('$project_content',''))";
 			mysqli_query($conn, $insert);
 			header('location:' . BASE_URL . '/home/projects');
 		}
