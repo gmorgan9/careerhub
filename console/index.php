e198431ee644c5632d524a2cc6292fc6301e9bf7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-28 06:26:36 -0500

    update

--- console/index.php
+++ console/index.php
@@ -114,6 +114,7 @@ foreach ($files as $file) {
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="margin-left: -50px;">
                             <a class="dropdown-item" href="/console/admin/add-job">Add Job</a>
+                            <a class="dropdown-item" href="/console/admin/update-job" disabled>Update Job</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="index.php?logout=1">Settings</a>
                         </div>
