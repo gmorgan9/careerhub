bb9bb06fc66b0dc6b3753a86437bbcde722b45ef
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-29 05:38:07 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -46,7 +46,7 @@ foreach ($files as $file) {
             <div class="row">
                 <div class=" col-xs-12 col-sm-12 ">
                     <ul class="project-filters nav nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
-                      <li class="nav-item" role="presentation">
+                      <li class="nav-item project-filter-item" role="presentation">
                         <button class="nav-link active project-filters-btn" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
                       </li>
                       <li class="nav-item" role="presentation">
