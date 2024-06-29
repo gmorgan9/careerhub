f02a003147609d27021021cf1bef3b536efcc651
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-29 09:51:11 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -62,7 +62,21 @@ $result = mysqli_query($conn, $sql);
                         <button class="nav-link" id="pills-open-source-tab" data-bs-toggle="pill" data-bs-target="#pills-open-source" type="button" role="tab" aria-controls="pills-open-source" aria-selected="false">Open Source</button>
                       </li>
                     </ul> -->
-                    <?php
+                    
+                    <ul class="project-filters nav nav-pills" id="pills-tab" role="tablist">
+                        <li class="nav-item project-filter-item" role="presentation">
+                            <button class="nav-link active" id="tab1-tab" data-bs-toggle="pill" data-bs-target="#tab1" type="button" role="tab" aria-controls="tab1" aria-selected="true">Tab 1</button>
+                        </li>
+                        <li class="nav-item project-filter-item" role="presentation">
+                            <button class="nav-link" id="tab2-tab" data-bs-toggle="pill" data-bs-target="#tab2" type="button" role="tab" aria-controls="tab2" aria-selected="false">Tab 2</button>
+                        </li>
+                        <li class="nav-item project-filter-item" role="presentation">
+                            <button class="nav-link" id="tab3-tab" data-bs-toggle="pill" data-bs-target="#tab3" type="button" role="tab" aria-controls="tab3" aria-selected="false">Tab 3</button>
+                        </li>
+                    </ul>
+                    <div class="tab-content project-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
+                        <?php
                     if (mysqli_num_rows($result) > 0) {
                         while ($row = mysqli_fetch_assoc($result)) {
                             $project_id            = $row['project_id'];
@@ -85,19 +99,6 @@ $result = mysqli_query($conn, $sql);
 
                     ?>
 
-                    <ul class="project-filters nav nav-pills" id="pills-tab" role="tablist">
-                        <li class="nav-item project-filter-item" role="presentation">
-                            <button class="nav-link active" id="tab1-tab" data-bs-toggle="pill" data-bs-target="#tab1" type="button" role="tab" aria-controls="tab1" aria-selected="true">Tab 1</button>
-                        </li>
-                        <li class="nav-item project-filter-item" role="presentation">
-                            <button class="nav-link" id="tab2-tab" data-bs-toggle="pill" data-bs-target="#tab2" type="button" role="tab" aria-controls="tab2" aria-selected="false">Tab 2</button>
-                        </li>
-                        <li class="nav-item project-filter-item" role="presentation">
-                            <button class="nav-link" id="tab3-tab" data-bs-toggle="pill" data-bs-target="#tab3" type="button" role="tab" aria-controls="tab3" aria-selected="false">Tab 3</button>
-                        </li>
-                    </ul>
-                    <div class="tab-content project-content" id="myTabContent">
-                        <div class="tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
                             <div class="row">
 
                                 <div class="col-md-6">
