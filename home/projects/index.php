ff888b7bebe21202ad8d5b6a158546b001e5c990
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-29 05:38:34 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -47,15 +47,15 @@ foreach ($files as $file) {
                 <div class=" col-xs-12 col-sm-12 ">
                     <ul class="project-filters nav nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
                       <li class="nav-item project-filter-item" role="presentation">
-                        <button class="nav-link active project-filters-btn" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+                        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
                       </li>
-                      <li class="nav-item" role="presentation">
+                      <li class="nav-item project-filter-item" role="presentation">
                         <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
                       </li>
-                      <li class="nav-item" role="presentation">
+                      <li class="nav-item project-filter-item" role="presentation">
                         <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
                       </li>
-                      <li class="nav-item" role="presentation">
+                      <li class="nav-item project-filter-item" role="presentation">
                         <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled" type="button" role="tab" aria-controls="pills-disabled" aria-selected="false" disabled>Disabled</button>
                       </li>
                     </ul>
