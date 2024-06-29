d88ba5859085937a8a53db4b256338be690a370e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-29 06:00:57 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -63,7 +63,9 @@ foreach ($files as $file) {
                 <div class="tab-content" id="pills-tabContent">
                   <div class="tab-pane fade show active" id="pills-all" role="tabpanel" aria-labelledby="pills-all-tab" tabindex="0">
 
-                  <img alt="https://i.ibb.co/tCZFDhL/Screen-Recording-2020-06-11-at-1.gif" id="cardHover" loading="lazy" width="500" height="500" decoding="async" data-nimg="1" class="" srcset="" style="color: transparent;" src="../../assets/images/project-images/EmergencyPrep.png">
+                  <div role="gridcell" id="cardHover" aria-label="Piggment The Gradients and colors for the next smart creator" tabindex="0" class="mansory-item__MansoryItemStyle-sc-luo7c4-0 gJKjnw" style="height: 310px;"><img alt="https://i.ibb.co/tCZFDhL/Screen-Recording-2020-06-11-at-1.gif" id="cardHover" loading="lazy" width="500" height="500" decoding="async" data-nimg="1" class="" srcset="/_next/image?url=https%3A%2F%2Fi.ibb.co%2FtCZFDhL%2FScreen-Recording-2020-06-11-at-1.gif&amp;w=640&amp;q=75 1x, /_next/image?url=https%3A%2F%2Fi.ibb.co%2FtCZFDhL%2FScreen-Recording-2020-06-11-at-1.gif&amp;w=1080&amp;q=75 2x" style="color: transparent;" src="/_next/image?url=https%3A%2F%2Fi.ibb.co%2FtCZFDhL%2FScreen-Recording-2020-06-11-at-1.gif&amp;w=1080&amp;q=75"><div class="content__slate"><h3>Piggment</h3><p>The Gradients and colors for the next smart creator</p><p class="d-flex flex-wrap"><span class="d-block mb-1">React</span><span class="d-block mb-1">Sass &amp; CSS</span><span class="d-block mb-1">Javascript</span><span class="d-block mb-1">Context</span></p></div></div>
+                    <img alt="https://i.ibb.co/tCZFDhL/Screen-Recording-2020-06-11-at-1.gif" id="cardHover" loading="lazy" width="500" height="500" decoding="async" data-nimg="1" class="" srcset="" style="color: transparent;" src="../../assets/images/project-images/EmergencyPrep.png">
+                  </div>
 
                   </div>
                   <div class="tab-pane fade" id="pills-web-dev" role="tabpanel" aria-labelledby="pills-web-dev-tab" tabindex="0">Web Development</div>
