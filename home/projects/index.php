500330bdace1f9d3128025dbb3364b1dce4cfe9c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-29 05:48:34 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -47,7 +47,7 @@ foreach ($files as $file) {
                 <div class=" col-xs-12 col-sm-12 ">
                     <ul class="project-filters nav nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
                       <li class="nav-item project-filter-item" role="presentation">
-                        <button class=" active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+                        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
                       </li>
                       <li class="nav-item project-filter-item" role="presentation">
                         <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
