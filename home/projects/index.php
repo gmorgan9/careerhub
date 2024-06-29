5ae1dde55a742a56642c2cba9bb10d2aa69acec4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-29 15:39:45 -0500

    update

--- home/projects/index.php
+++ home/projects/index.php
@@ -121,7 +121,7 @@ if ($conn->connect_error) {
                                                             <!-- <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button> -->
                                                         </div>
                                                         <div class="offcanvas-body">
-                                                            <button type="button" data-bs-dismiss="offcanvas" aria-label="Close"><i class="bi bi-arrow-left-circle"></i></button>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"><i class="bi bi-arrow-left-circle"></i></button>
                                                             <hr>
                                                             <?php echo $project_name; ?>
                                                         </div>
