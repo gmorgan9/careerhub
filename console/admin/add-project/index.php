05a8a0a7f5af16d8e6bdd00e5fed68804e0e08f0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-06-29 17:26:05 -0500

    update

--- console/admin/add-project/index.php
+++ console/admin/add-project/index.php
@@ -43,7 +43,7 @@ foreach ($files as $file) {
                 Add Project
             </h2>
         </div>
-    <div class="container-fluid" style="max-width: 1320px; margin: 0 auto; height: 100vh;">
+    <div class="container-fluid" style="max-width: 1320px; margin: 0 auto; height: calc(100vh - 100px);">
         
         <div class="form">
             <form action="" method="POST">
