99bab75e383dde20894773803e7c54cca38582ec
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-01 06:44:18 -0500

    update

--- console/admin/add-experience/index.php
+++ console/admin/add-experience/index.php
@@ -87,8 +87,14 @@ foreach ($files as $file) {
                         Active 3
                       </label>
                     </div> -->
-                    <textarea id="ce_job_duties" name="ce_job_duties" class="form-input mb-3" placeholder="Experience Job Duties" rows="4"></textarea>
-                    <textarea id="ce_notes" name="ce_notes" class="form-input mb-3" placeholder="Experience Notes" rows="2"></textarea>
+                    <div class="form-group">
+                        <label class="form-label text-white" for="ce_job_duties">Experience Job Duties <span class="text-danger" style="font-size: 8px; vertical-align: top;">&nbsp;<i class="bi bi-asterisk"></i></span></label>
+                        <textarea id="ce_job_duties" name="ce_job_duties" class="form-input mb-3"  rows="4" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label text-white" for="ce_notes">Experience Notes <span class="text-secondary" style="font-size: 12px;">&nbsp;&nbsp;Optional</span></label>
+                        <textarea id="ce_notes" name="ce_notes" class="form-input mb-3" rows="2"></textarea>
+                    </div>
                     <input type="submit" name="add-experience" class="form-btn mt-2" value="Add Certification">
                 </div>
             </form>
