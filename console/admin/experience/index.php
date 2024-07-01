fd91fce529ed33c102a0aba9eed974e473effb59
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-01 07:54:51 -0500

    update

--- console/admin/experience/index.php
+++ console/admin/experience/index.php
@@ -38,10 +38,10 @@ $result = mysqli_query($conn, $sql);
     <?php include(ROOT_PATH . "/app/database/includes/console-header.php"); ?>
 
     <div class="page_title">   
-        <div class="float-start">
+        <div class="me-auto">
             <h2 class="text-white title">Career Experience</h2>
         </div> 
-        <div class="me-auto">
+        <div class="ms-auto">
             <a href="<?php echo BASE_URL; ?>/console/admin/expereince/add-experience">Add Experience</a>
         </div>
     </div>
