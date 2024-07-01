efcdb6f5c51e622b5c0b6fcebdcc54adb7bd428e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-01 06:47:01 -0500

    update

--- console/admin/add-experience/index.php
+++ console/admin/add-experience/index.php
@@ -68,6 +68,7 @@ foreach ($files as $file) {
                             <input id="ce_end" type="text" name="ce_end" class="form-input">
                         </div> 
                     </div>
+                </div>
                     <!-- <legend class="text-white" for="form-checks" style="font-size: 16px !important;">Active Career Experience <span class="text-danger" style="font-size: 8px; vertical-align: top;">&nbsp;<i class="bi bi-asterisk"></i></span></legend>
                     <div class="form-check form-check-inline text-white">
                       <input class="form-check-input" type="radio" name="ce_status" id="ce_status1" value="active-1">
@@ -87,16 +88,15 @@ foreach ($files as $file) {
                         Active 3
                       </label>
                     </div> -->
-                    <div class="form-group">
-                        <label class="form-label text-white" for="ce_job_duties">Experience Job Duties <span class="text-danger" style="font-size: 8px; vertical-align: top;">&nbsp;<i class="bi bi-asterisk"></i></span></label>
-                        <textarea id="ce_job_duties" name="ce_job_duties" class="form-input mb-3"  rows="4" required></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label class="form-label text-white" for="ce_notes">Experience Notes <span class="text-secondary" style="font-size: 12px;">&nbsp;&nbsp;Optional</span></label>
-                        <textarea id="ce_notes" name="ce_notes" class="form-input mb-3" rows="2"></textarea>
-                    </div>
-                    <input type="submit" name="add-experience" class="form-btn mt-2" value="Add Experience">
+                <div class="form-group">
+                    <label class="form-label text-white" for="ce_job_duties">Experience Job Duties <span class="text-danger" style="font-size: 8px; vertical-align: top;">&nbsp;<i class="bi bi-asterisk"></i></span></label>
+                    <textarea id="ce_job_duties" name="ce_job_duties" class="form-input mb-3"  rows="4" required></textarea>
                 </div>
+                <div class="form-group">
+                    <label class="form-label text-white" for="ce_notes">Experience Notes <span class="text-secondary" style="font-size: 12px;">&nbsp;&nbsp;Optional</span></label>
+                    <textarea id="ce_notes" name="ce_notes" class="form-input mb-3" rows="2"></textarea>
+                </div>
+                <input type="submit" name="add-experience" class="form-btn mt-2" value="Add Experience">
             </form>
         </div> 
     </div>
