47adef29d92f36bbc0f501ce49d0f4b410f32711
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-01 08:49:34 -0500

    update

--- app/functions/update_experience.php
+++ app/functions/update_experience.php
@@ -11,19 +11,24 @@ if (isset($_POST['update-experience'])) {
     $ce_status = isset($_POST['ce_status']) ? mysqli_real_escape_string($conn, $_POST['ce_status']) : "";
     $ce_notes = isset($_POST['ce_notes']) ? mysqli_real_escape_string($conn, $_POST['ce_notes']) : "";
 
-    $update = "UPDATE career_experience SET 
-                ce_company = NULLIF('$ce_company', ''), 
-                ce_start = NULLIF('$ce_start', ''), 
-                ce_end = NULLIF('$ce_end', ''), 
-                ce_job_title = NULLIF('$ce_job_title', ''), 
-                ce_job_duties = NULLIF('$ce_job_duties', ''), 
-                ce_status = NULLIF('$ce_status', ''), 
-                ce_notes = NULLIF('$ce_notes', '')
-               WHERE ce_id = '$id';";
-    
-    mysqli_query($conn, $update);
-    header('location:' . BASE_URL . '/console/admin/experience');
+    $select = "SELECT * FROM career_experience WHERE idno = '$idno'";
+    $result = mysqli_query($conn, $select);
+    if (mysqli_num_rows($result) > 0) {
+        $error[] = 'Job already exists!';
+    } else {
+        $update = "UPDATE career_experience SET 
+                    ce_company = NULLIF('$ce_company', ''), 
+                    ce_start = NULLIF('$ce_start', ''), 
+                    ce_end = NULLIF('$ce_end', ''), 
+                    ce_job_title = NULLIF('$ce_job_title', ''), 
+                    ce_job_duties = NULLIF('$ce_job_duties', ''), 
+                    ce_status = NULLIF('$ce_status', ''), 
+                    ce_notes = NULLIF('$ce_notes', '')
+                   WHERE ce_id = '$id';";
 
+        mysqli_query($conn, $update);
+        header('location:' . BASE_URL . '/console/admin/experience');
+    }
 }
 
 ?>
