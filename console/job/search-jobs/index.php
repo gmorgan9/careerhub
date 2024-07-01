c7ee3fe9f422fc347d6947f1b7179bab28b8c724
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-01 10:24:54 -0500

    update

--- console/job/search-jobs/index.php
+++ console/job/search-jobs/index.php
@@ -1,12 +1,12 @@
 <?php
 // Include necessary files and start session
 date_default_timezone_set('America/Denver');
-require_once "../../app/database/connection.php";
-require_once "../../path.php";
+require_once "../../../app/database/connection.php";
+require_once "../../../path.php";
 session_start();
 
 // Include all PHP functions
-$files = glob("../../app/functions/*.php");
+$files = glob("../../../app/functions/*.php");
 foreach ($files as $file) {
     require_once $file;
 }
@@ -66,7 +66,7 @@ foreach ($files as $file) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../assets/css/home.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../../../assets/css/home.css?v=<?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
