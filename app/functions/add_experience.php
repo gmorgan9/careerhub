617a0780db720a4d2a963bb1f200dcec3c47c5dc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-01 11:11:33 -0500

    update

--- app/functions/add_experience.php
+++ app/functions/add_experience.php
@@ -8,7 +8,7 @@ if (isset($_POST['add-experience'])) {
     if(isset($_POST['ce_end'])) { $ce_end = mysqli_real_escape_string($conn, $_POST['ce_end']); } else { $ce_end = ""; }
     if(isset($_POST['ce_job_title'])) { $ce_job_title = mysqli_real_escape_string($conn, $_POST['ce_job_title']); } else { $ce_job_title = ""; }
     if(isset($_POST['ce_job_duties'])) { $ce_job_duties = mysqli_real_escape_string($conn, $_POST['ce_job_duties']); } else { $ce_job_duties = ""; }
-    if(isset($_POST['ce_status'])) { $ce_status = mysqli_real_escape_string($conn, $_POST['ce_status']); } else { $ce_status = ""; }
+    // if(isset($_POST['ce_status'])) { $ce_status = mysqli_real_escape_string($conn, $_POST['ce_status']); } else { $ce_status = ""; }
     if(isset($_POST['ce_notes'])) { $ce_notes = mysqli_real_escape_string($conn, $_POST['ce_notes']); } else { $ce_notes = ""; }
     
 
@@ -18,7 +18,7 @@ if (isset($_POST['add-experience'])) {
     if (mysqli_num_rows($result) > 0) {
         $error[] = 'The experience already exists!';
     } else {
-        $insert = "INSERT INTO career_experience (idno, ce_company, ce_start, ce_end, ce_job_title, ce_job_duties, ce_status, ce_notes) VALUES ('$idno', NULLIF('$ce_company',''), NULLIF('$ce_start',''), NULLIF('$ce_end',''), NULLIF('$ce_job_title',''), NULLIF('$ce_job_duties',''), NULLIF('$ce_status',''), NULLIF('$ce_notes',''))";
+        $insert = "INSERT INTO career_experience (idno, ce_company, ce_start, ce_end, ce_job_title, ce_job_duties, ce_notes) VALUES ('$idno', NULLIF('$ce_company',''), NULLIF('$ce_start',''), NULLIF('$ce_end',''), NULLIF('$ce_job_title',''), NULLIF('$ce_job_duties',''), NULLIF('$ce_notes',''))";
         mysqli_query($conn, $insert);
         header('location:' . BASE_URL . '/console/experience');
     }
